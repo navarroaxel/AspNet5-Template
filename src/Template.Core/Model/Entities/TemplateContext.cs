c6e94c6c0f85bdd38fdb0529bcef92b65995bdeb
Axel Navarro <navarroaxel@gmail.com>
2015-08-09 19:54:03 -0300

    +core project

--- src/Template.Core/Model/Entities/TemplateContext.cs
+++ src/Template.Core/Model/Entities/TemplateContext.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Template.Core.Model.Entities
 {
-    public class TemplateContext:DbContext
+    internal class TemplateContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
     }
 }
--- src/Template.Core/Model/Entities/User.cs
+++ src/Template.Core/Model/Entities/User.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Template.Core.Model.Entities
 {
+    [Table("Users")]
     public class User
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
